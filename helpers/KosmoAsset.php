397df5f85d73fe2a27691d5389ec685e15c2f09f
renjelc <renjelc@gmail.com>
2017-11-11 17:04:42 +0800

    Kosmo Helpers update

--- helpers/KosmoAsset.php
+++ helpers/KosmoAsset.php
@@ -1,5 +1,5 @@
 <?php
-namespace renj1e\helpers;
+namespace backend\assets\kosmo;
 
 use yii\base\Exception;
 use yii\web\AssetBundle as BaseKosmoAsset;
@@ -20,31 +20,38 @@ class KosmoAsset extends BaseKosmoAsset
         'libs/flag-icon-css/css/flag-icon.min.css',
         'assets/styles/common.min.css',
 
-        'assets/styles/themes/primary.min.css',
+        'assets/styles/themes/jungle-green.min.css',
         'assets/styles/themes/sidebar-black.min.css',
+
+        'assets/styles/widgets/panels.min.css',
+        'assets/scripts/charts/area/area.chart.min.css',
+        'assets/scripts/charts/radial-progress/radial-progress.chart.min.css',
+        // 'assets/fonts/kosmo/styles.css',
+        // 'assets/fonts/weather/css/weather-icons.min.css',
+        // 'libs/c3js/c3.min.css',
+        // 'libs/noty/noty.css',
+        // 'assets/styles/widgets/payment.min.css',
+        // 'assets/styles/dashboard/tabbed-sidebar.min.css',
     ];
     public $js = [
+        'libs/jquery/jquery.min.js',
         'libs/responsejs/response.min.js',
         'libs/loading-overlay/loadingoverlay.min.js',
         'libs/tether/js/tether.min.js',
-        'libs/bootstrap/js/bootstrap.min.js'
+        'libs/bootstrap/js/bootstrap.min.js',
         'libs/jscrollpane/jquery.jscrollpane.min.js',
         'libs/jscrollpane/jquery.mousewheel.js',
         'libs/flexibility/flexibility.js',
-        
         'assets/scripts/common.min.js',
+
+        // 'libs/d3/d3.min.js',
+        // 'libs/c3js/c3.min.js',
+        // 'libs/noty/noty.min.js',
+        // 'libs/maplace/maplace.min.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
+        // 'yii\web\YiiAsset',
         // 'yii\bootstrap\BootstrapAsset',
         // 'yii\bootstrap\BootstrapPluginAsset',
     ];
-    
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        parent::init();
-    }
 }
---
+++ helpers/KosmoSrcAsset.php
@@ -0,0 +1,18 @@
+<?php
+namespace backend\assets\kosmo;
+
+use yii\base\Exception;
+use yii\web\AssetBundle as BaseKosmoAsset;
+
+/**
+ * AdminLte AssetBundle
+ * @since 0.1
+ */
+class KosmoSrcAsset extends BaseKosmoAsset
+{
+    public $sourcePath = '@vendor/renj1e/yii2-kosmo-template/src';
+    public $css = [
+    ];
+    public $js = [
+    ];
+}
